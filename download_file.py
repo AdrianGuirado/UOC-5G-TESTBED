66e3f0a8be4a2397ad6817709142019138155685
Adrián <adrianguiradoluna@gmail.com>
2024-11-20 03:59:54 +0100

    Update

---
+++ download_file.py
@@ -0,0 +1,18 @@
+import urllib.request
+
+def download_file_from_ip(ip, port, file_path, save_path):
+
+    url = f"http://{ip}:{port}/{file_path}"
+    try:
+        print(f"Connecting to {ip} and downloading from: {url}")
+        urllib.request.urlretrieve(url, save_path)
+        print(f"Downloaded in: {save_path}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+ip_address = "192.168.151.152"
+port = 8080
+remote_file_path = "test_download.txt"
+local_save_path = "txtdownloaded.txt"  
+
+download_file_from_ip(ip_address, port, remote_file_path, local_save_path)
